a5b996d7db9e629f24729e0ae8a654e9255f2cd7
jaimenguyen168 <77992599+jaimenguyen168@users.noreply.github.com>
2024-03-08 14:37:47 -0500

    Initial Commit

---
+++ SwiftUi-Threads/SwiftUi_ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_ThreadsApp.swift
+//  SwiftUi-Threads
+//
+//  Created by Dat Nguyen on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
